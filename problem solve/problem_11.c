f6d22b6156b94be755efef54475b078e29a9d1f9
maruf-star <marufraj2008@gmail.com>
2026-08-10 01:59:03 +0600

    Fix input format for reading two integers
    Updated scanf to read two numbers with space separation.

--- problem solve/problem_11.c
+++ problem solve/problem_11.c
@@ -2,7 +2,7 @@
 int main(void){
     int a,b;
     printf("Enter two number: ");
-    scanf("%d%d",&a,&b);
+    scanf("%d %d",&a,&b);
     printf("Remainder of two number:%d\n",a%b);
     
     return 0;
